64bd87d43ede1ea2975f7b52cb91d23bb7e51168
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2021-06-15 23:37:26 +0100

    Added my project

---
+++ serverdiamond.js
@@ -0,0 +1,13 @@
+var express = require("express");
+const path = require("path");
+const app = express();
+var cors = require("cors");
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(express.json());
+
+app.use(cors());
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(3304);
